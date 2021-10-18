baa2cdeaa5185b951965b57e0ba4840bb5262e14
Reza Firdaus <azerus98@gmail.com>
2021-10-18 11:18:55 +0700

    first commit

---
+++ spec/models/auction_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Auction, type: :model do
+	let(:user) {
+		User.new(email: 'a@gmail.com', password: '1234')
+	}
+	subject {
+    described_class.new(title: "Anything",
+                        description: "Lorem ipsum",
+                        start_date: DateTime.now,
+                        end_date: DateTime.now + 1.week)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a start_date" do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a end_date" do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
+
+	describe "Association" do
+		it { should belong_to(:user).without_validating_presence }
+		it { should have_many(:bids) }
+	end
+end
